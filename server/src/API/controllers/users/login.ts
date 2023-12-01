2e39d9f7b7dc91b13a8818759fcecd32c7df63b5
hamisirizwan <hamisirizwan5@gmail.com>
2023-12-01 18:46:25 +0300

    modified login endpoint to accept email or phone

--- server/src/API/controllers/users/login.ts
+++ server/src/API/controllers/users/login.ts
@@ -1,53 +1,79 @@
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import prisma from '../../DB/prisma';
-import missingRequiredParams from '../../utilities/validateRequiredInputs';
-import { Request, Response } from 'express';
-
-
-
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import prisma from "../../DB/prisma";
+import missingRequiredParams from "../../utilities/validateRequiredInputs";
+import { Request, Response } from "express";
 
 const login = async (req: Request, res: Response) => {
-  const { phone, password } = req.body;
+  const { identifier, password } = req.body;
   try {
-    const missingField = missingRequiredParams(["phone", "password"], req.body);
+    const missingField = missingRequiredParams(["identifier", "password"], req.body);
     if (missingField) {
-      return res.status(400).json({message:`${missingField} is required`, status:400});
+      return res
+        .status(400)
+        .json({ message: `${missingField} is required`, status: 400 });
     }
 
     // find user
     const existingUser = await prisma.users.findFirst({
       where: {
-        phone_number: phone,
+        OR: [
+          {
+            phone_number: identifier,
+          },
+          {
+            email: identifier,
+          },
+        ],
+      },
+      include: {
+        user_roles: {
+          include: {
+            roles: {
+              include: {
+                role_permissions: true,
+              },
+            },
+          },
+        },
       },
     });
 
     if (!existingUser) {
-      return res.status(400).json({message:`user with phone: ${phone} not found`, status:400});
+      return res
+        .status(400)
+        .json({ message: `Invalid email or phone number`, status: 400 });
     }
 
-    if(!existingUser.is_approved){
-        return res.status(403).json({message:"User pending approval", status:403})
+    if (!existingUser.is_approved) {
+      return res
+        .status(403)
+        .json({ message: "User pending approval", status: 403 });
     }
 
-    if(!existingUser.is_active){
-      return res.status(403).json({message:"User inactive", status:403})
-  }
+    if (!existingUser.is_active) {
+      return res.status(403).json({ message: "User inactive", status: 403 });
+    }
     // validate password
-    const validPassword = await bcrypt.compare(password, existingUser.password!);
+    const validPassword = await bcrypt.compare(
+      password,
+      existingUser.password!
+    );
     if (!validPassword) {
-      return res.status(400).json({message:"wrong password" , status:400});
+      return res.status(400).json({ message: "wrong password", status: 400 });
     }
-    existingUser.password = '#############';
+    existingUser.password = "#############";
 
     const token = jwt.sign(existingUser, process.env.JWTSECRET!, {
-      expiresIn: '3d',
+      expiresIn: "3d",
     });
 
-    res.status(200).json({ message: 'log in successful', user: existingUser, token });
-  } catch (error:any) {
+    res
+      .status(200)
+      .json({ message: "log in successful", user: existingUser, token });
+  } catch (error: any) {
     console.log(error);
-    res.status(500).json({message:error.message, status:500});
+    res.status(500).json({ message: error.message, status: 500 });
   }
 };
 
--- server/src/API/middlewares/auth.ts
+++ server/src/API/middlewares/auth.ts
@@ -1,56 +1,60 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
-import asyncHandler from 'express-async-handler';
-import prisma from '../DB/prisma';
-
-
-
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import prisma from "../DB/prisma";
 
 // Extend the Request object to add a 'user' property
 
-type apiREQ = Request & {user?:any}
+type apiREQ = Request & { user?: any };
 
-const Auth = asyncHandler(async (req: apiREQ, res: Response, next: NextFunction) => {
-  let token;
+const Auth = asyncHandler(
+  async (req: apiREQ, res: Response, next: NextFunction) => {
+    let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    try {
-      token = req.headers.authorization.split(' ')[1];
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      try {
+        token = req.headers.authorization.split(" ")[1];
 
-    
-      const decoded: any = jwt.verify(token, process.env.JWTSECRET!);
+        const decoded: any = jwt.verify(token, process.env.JWTSECRET!);
 
-      req.user = await prisma.users.findUnique({
-        where: {
-          phone_number: decoded.phone_number,
-        },
-        include:{
-          user_roles:{
-           include:{
-             roles:{
-               include:{
-                 role_permissions:true
-               }
-            
-             }
-           }
-          }
-       }
-      });
+        req.user = await prisma.users.findFirst({
+          where: {
+            OR: [
+              { phone_number: decoded.phone_number },
+              { email: decoded.email },
+            ],
+          },
+          include: {
+            user_roles: {
+              include: {
+                roles: {
+                  include: {
+                    role_permissions: true,
+                  },
+                },
+              },
+            },
+          },
+        });
 
-      next();
-    } catch (error) {
-      console.error(error);
-      res.status(401).json({message:'Not authorized, token failed', status:401});
+        next();
+      } catch (error) {
+        console.error(error);
+        res
+          .status(401)
+          .json({ message: "Not authorized, token failed", status: 401 });
+      }
+    }
+
+    if (!token) {
+      res
+        .status(401)
+        .json({ message: "Not authorized, no token", status: 401 });
     }
   }
+);
 
-  if (!token) {
-    res.status(401).json({message:'Not authorized, no token', status:401});
-  }
-});
-
-export default Auth
+export default Auth;
