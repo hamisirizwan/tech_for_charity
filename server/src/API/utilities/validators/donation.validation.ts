646de5b3939d1c2dee19abbfb3d5e59dd424e06c
hamisirizwan <hamisirizwan5@gmail.com>
2024-01-14 18:19:22 +0300

    modified donation joi schema

--- server/src/API/utilities/validators/donation.validation.ts
+++ server/src/API/utilities/validators/donation.validation.ts
@@ -5,5 +5,5 @@ const kenyanPhoneNumberRegex = /^(07\d{8}|01\d{8}|2547\d{8}|2541\d{8}|\+2547\d{8
 export const donationSchema = Joi.object({
   name: Joi.string().required(),
   amount: Joi.number().required(),
-  phoneNumber: Joi.string().regex(kenyanPhoneNumberRegex).required(),
+  phoneNumber: Joi.string().regex(kenyanPhoneNumberRegex).message('Invalid Kenyan phone number format').required(),
 });
